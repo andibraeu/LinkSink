41e9e9763ba6e4a2d9794786c2459b5972afcb04
andibraeu <ab@andi95.de>
2014-09-04 18:07:28 +0200

    made bookmarklet more robust and improved content parsing

--- web/js/bookmarklet/bookmarklet.js
+++ web/js/bookmarklet/bookmarklet.js
@@ -9,12 +9,43 @@
   
   // jQuery should have been loaded by the wrapper
   if (!$().jquery) { fail("No jQuery found! It's a bug, please report it."); }
+
+  //SiteName  
+  var mySiteName = null;
+  if ( $('meta[property="og:site_name"]').length>0) {
+    mySiteName = $('head').find('meta[property="og:site_name"]').prop('content');
+  }
+
+  //Title 
+  if ( $('meta[property="og:title"]').length>0) {
+    var myTitle = $('meta[property="og:title"]').attr('content');
+  } else {
+    var myTitle = document.title;
+  }
+
+  //URL
+  if ( $('meta[property="og:url"]').length>0) {
+    var myUrl = $('meta[property="og:url"]').attr('content');
+  } else {
+    var myUrl = window.location.href;
+  }
+
+  //Description
+  var myDescription = null;
+  if ( $('meta[property="og:description"]').length>0) {
+    var myDescription = $('meta[property="og:description"]').attr('content');
+  }
+
+  if (mySiteName) {
+    myTitle = mySiteName + ": " + myTitle;
+  }
   
   // get some info about the current page
   var link = {
-    title: document.title || "(Unknown Title)",
-    url: window.location.href || null,
-    date: new Date($('head').find('meta[name="date"]').prop('content'))
+    title: myTitle || "(Unknown Title)",
+    url: myUrl || null,
+    description: myDescription || null,
+    date: new Date($('meta[name="date"]').attr('content'))
   };
   
   // a missing URL is a hard error
@@ -35,6 +66,7 @@
   var submitURL = apiURL+
     '?url='+encodeURIComponent(link.url)+
     '&title='+encodeURIComponent(link.title)+
+    '&description='+encodeURIComponent(link.description)+
     '&date='+link.date;
     
   // redirect to the submit form
