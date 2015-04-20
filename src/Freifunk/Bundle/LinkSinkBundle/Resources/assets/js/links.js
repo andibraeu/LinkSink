9839ffef07028460c4b4b075e7b420258f3aa262
andibraeu <ab@andi95.de>
2015-04-20 16:55:04 +0200

    worked on semantic ui upgrade

--- src/Freifunk/Bundle/LinkSinkBundle/Resources/assets/js/links.js
+++ src/Freifunk/Bundle/LinkSinkBundle/Resources/assets/js/links.js
@@ -7,8 +7,35 @@
 function addGeoCoordinates(ev) {
     return false;
 }
+
+function calcBoxSize(columns) {
+    var card_selector = jQuery('.ui.cards .card');
+    var screen_width = $(window).width() - 14 - 14; /* padding of basic segment */
+    // first check if we can display 4 cards on the screen with a minimum width of 399px
+    var box_width = Math.floor((screen_width / columns)) - 10;
+    if ((box_width >= 395) || (columns == 1)) {
+        card_selector.css('width',box_width);
+    } else {
+        calcBoxSize(columns - 1);
+    }
+}
+
+$(window).resize(function(){
+    var card_selector = jQuery('.ui.cards .card');
+
+    if (card_selector.length > 0) {
+        calcBoxSize(4);
+    }
+})
+
 var map = null;
 jQuery(document).ready(function () {
+    var card_selector = jQuery('.ui.cards .card');
+
+    if (card_selector.length > 0) {
+        calcBoxSize(4);
+    }
+
     if (jQuery('.icon.link').length > 0) {
         jQuery('.icon.link').popup();
     }
@@ -18,3 +45,5 @@ jQuery(document).ready(function () {
     }
 
 });
+
+
